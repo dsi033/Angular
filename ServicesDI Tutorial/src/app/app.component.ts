e69a751d8ab0b4b9e0941966e73b69f4f0f757c0
dsi033 <adrianalejomoya2004@gmail.com>
2025-03-27 13:46:56 +0100

    Add files via upload

--- ServicesDI Tutorial/src/app/app.component.ts
+++ ServicesDI Tutorial/src/app/app.component.ts
@@ -1,18 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MessagesService } from './services/messages.service';
+import { Post } from './interfaces/posts.interface';
+import { LatestPrices, OrderBook, Student } from './interfaces/data.interface';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [MessagesService]
+  providers: [MessagesService],
 })
-export class AppComponent {
-  messages: string[] = []
-
+export class AppComponent implements OnInit {
+  messages: string[] = [];
+  posts: Post[] = [];
   constructor(private messagesService: MessagesService) {
     this.messages = messagesService.getMessages();
   }
+
+  ngOnInit() {
+
+    /*
+         this.messagesService.getPosts().subscribe(response => {
+          this.posts = response;
+        }, (error) => {
+          console.error(error);
+        });
+    */
+    this.messagesService.getPosts().subscribe({
+      next: (response: Post[]) => {
+        this.posts = response;
+      },
+      error: (error) => { console.error(error); }
+    });
+
+    this.messagesService.getLanguages().subscribe({
+      next: (languages: Array<string>) => {
+        console.log(languages);['Java', 'C#', 'Python']
+      },
+    });
+
+    this.messagesService.getStudent().subscribe({
+      next: (student: Student) => {
+        console.log(student);
+      },
+    });
+
+    this.messagesService.getBookOrders().subscribe({
+      next: (bookOrders: OrderBook) => {
+        console.log(bookOrders);
+      },
+    });
+
+    this.messagesService.getLatestPrices().subscribe({
+      next: (latestprices: LatestPrices) => {
+        console.log(latestprices);
+      },
+    });
+
+
+  }
+
   title = 'ServicesDI';
 }
--- ServicesDI Tutorial/src/app/services/messages.service.ts
+++ ServicesDI Tutorial/src/app/services/messages.service.ts
@@ -1,13 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from '../interfaces/posts.interface';
+import { LatestPrices, OrderBook, Student } from '../interfaces/data.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessagesService {
 
-  constructor() { }
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts'
 
-  getMessages(){
+  constructor(private http: HttpClient) { }
+
+  getMessages() {
     return ['Message 1', 'Message 2', 'Message 3'];
+  }
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.apiUrl)
+  }
+
+  getLanguages(): Observable<Array<string>> {
+    return this.http.get<Array<string>>('http://localhost:1111/languages')
+  }
+
+  getStudent(): Observable<Student> {
+    return this.http.get<Student>('http://localhost:1111/student')
+  }
+
+  getBookOrders(): Observable<OrderBook> {
+    return this.http.get<OrderBook>('http://localhost:1111/orderbook')
+  }
+
+  getLatestPrices(): Observable<LatestPrices> {
+    return this.http.get<LatestPrices>('http://localhost:1111/latestprices')
   }
 }
